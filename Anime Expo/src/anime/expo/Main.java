b2eef7f695a793092f80cfc8a44c7954421f96bd
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-08-25 09:51:19 -0500

    feat: :sparkles: Root folder
    Folder

---
+++ Anime Expo/src/anime/expo/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package anime.expo;
+
+/**
+ *
+ * @author Juan Felipe Rubio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
