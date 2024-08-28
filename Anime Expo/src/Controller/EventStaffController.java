5cf3b5b0dea3cc45c6a2f09c2e9735f9954c6582
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-08-28 04:00:48 -0500

    feat: :white_check_mark: Finalization of the event staff class
    Completed

--- Anime Expo/src/Controller/EventStaffController.java
+++ Anime Expo/src/Controller/EventStaffController.java
@@ -4,10 +4,35 @@
  */
 package Controller;
 
+// folder import
+import Model.EventStaff;
+import Dao.EventStaffDao;
+
+import java.util.List;
+
 /**
  *
  * @author Juan Felipe Rubio
  */
+
 public class EventStaffController {
-    
+        
+ private EventStaffDao eventStaffDao = new EventStaffDao();
+
+    public boolean isEventNameExists(String name) {
+        List<EventStaff> eventstaffs = eventStaffDao.ViewEvent();
+        for (EventStaff eventstaff : eventstaffs) {
+            if (eventstaff.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<EventStaff> getAllEventsStaff() {
+        return eventStaffDao.ViewEvent();
+    }
+    public boolean AddEventStaff(EventStaff eventstaff){
+        return eventStaffDao.InsertEventSatff(eventstaff); 
+    }
 }
--- Anime Expo/src/Model/EventStaff.java
+++ Anime Expo/src/Model/EventStaff.java
@@ -4,10 +4,100 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author Juan Felipe Rubio
  */
+
 public class EventStaff {
+    private int staff_id;
+    private String name;
+    private String id_number;
+    private Date birth_date;
+    private String role;
+    private String status;
+    private int event_id;
     
+    public EventStaff(int staff_id,String name, String id_number,Date birth_date,String role,String status, int event_id){
+    this.staff_id =staff_id;
+    this.name = name;
+    this.id_number = id_number;
+    this.birth_date = birth_date;
+    this.role = role;
+    this.status = status;
+    this.event_id = event_id;
+    }
+    
+    // Getters
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId_number() {
+        return id_number;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    // Setters
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId_number(String id_number) {
+        this.id_number = id_number;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+    // Override toString method
+    @Override
+    public String toString() {
+        return "EventStaff{" +
+                "staff_id=" + staff_id +
+                ", name='" + name + '\'' +
+                ", id_number='" + id_number + '\'' +
+                ", birth_date=" + birth_date +
+                ", role='" + role + '\'' +
+                ", status='" + status + '\'' +
+                ", event_id=" + event_id +
+                '}';
+    }
 }
