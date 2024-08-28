5188e0f3786d40f18696ec61f8e187c314fa4dce
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-08-28 03:59:23 -0500

    feat: :white_check_mark: Finalization of the visitor class
    Completed

--- Anime Expo/src/Controller/VisitorFeedbackController.java
+++ Anime Expo/src/Controller/VisitorFeedbackController.java
@@ -4,10 +4,38 @@
  */
 package Controller;
 
+// folder import
+import Model.VisitorFeedback;
+import Dao.VisitorFeedbackDao;
+
+import java.util.List;
+
 /**
  *
  * @author Juan Felipe Rubio
  */
+
 public class VisitorFeedbackController {
     
+    private VisitorFeedbackDao visitorfeedbackDao = new VisitorFeedbackDao();
+
+     
+    public boolean isVisitorFeedbackExists(int event_id) {
+    List<VisitorFeedback> visitorfeedbacks = visitorfeedbackDao.ViewVisitorFeedback();
+    for (VisitorFeedback visitorfeedback : visitorfeedbacks) {
+        if (visitorfeedback.getEvent_id() == event_id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+    public List<VisitorFeedback> getAllVisitorFeedback() {
+        return visitorfeedbackDao.ViewVisitorFeedback();
+    }
+    
+    public boolean AddVisitorFeedback(VisitorFeedback visitorfeedback){
+        return visitorfeedbackDao.InsertVisitorFeedback(visitorfeedback); 
+    }
 }
