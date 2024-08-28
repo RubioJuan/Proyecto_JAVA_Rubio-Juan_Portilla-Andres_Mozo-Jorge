800141f6d65a2414bd720d956e3b53744aeb23e9
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-08-28 04:01:20 -0500

    feat: :white_check_mark: Finalization of the participant class
    Completed

--- Anime Expo/src/Controller/ParticipantController.java
+++ Anime Expo/src/Controller/ParticipantController.java
@@ -4,10 +4,35 @@
  */
 package Controller;
 
+// folder import
+import Model.Participant;
+import Dao.ParticipantDao;
+
+import java.util.List;
+
 /**
  *
  * @author Juan Felipe Rubio
  */
+
 public class ParticipantController {
     
+     private ParticipantDao participantDao = new ParticipantDao();
+
+      public boolean isEventNameExists(String name) {
+        List<Participant> participants = participantDao.ViewParticipant();
+        for (Participant eventstaff : participants) {
+            if (eventstaff.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Participant> getAllEventsStaff() {
+        return participantDao.ViewParticipant();
+    }
+    public boolean AddEventStaff(Participant participant){
+        return participantDao.InsertParticipant(participant); 
+    }
 }
--- Anime Expo/src/Model/Participant.java
+++ Anime Expo/src/Model/Participant.java
@@ -8,6 +8,42 @@ package Model;
  *
  * @author Juan Felipe Rubio
  */
+
 public class Participant {
+    private int participant_id;
+    private String name;
+    private int activity_id;
     
+    public Participant(int participant_id, String name, int activity_id){
+    this.participant_id = participant_id;
+    this.name = name;
+    this.activity_id = activity_id;
+    }
+
+    // Getter y Setter para participant_id
+    public int getParticipant_id() {
+        return participant_id;
+    }
+
+    public void setParticipant_id(int participant_id) {
+        this.participant_id = participant_id;
+    }
+
+    // Getter y Setter para name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter y Setter para activity_id
+    public int getActivity_id() {
+        return activity_id;
+    }
+
+    public void setActivity_id(int activity_id) {
+        this.activity_id = activity_id;
+    }
 }
