6f3249203dfcacb40de92af9fb13e656eb556a32
Jorge Mozo <jorgy29081@gmail.com>
2024-08-28 14:57:40 -0500

    feat: :zap: Create Store Entity
    maked a all menu about the Store

--- Anime Expo/src/Controller/StoreController.java
+++ Anime Expo/src/Controller/StoreController.java
@@ -4,10 +4,56 @@
  */
 package Controller;
 
+import Model.Store;
+import Dao.StoreDao;
+import java.util.List;
+
+
 /**
  *
- * @author Juan Felipe Rubio
+ * @author Jorge Luis Mozo
  */
 public class StoreController {
+    private StoreDao storeDao;
     
+    public StoreController(){
+        this.storeDao =  new StoreDao();
+    }
+    
+    public void createStore(Store store) {
+        try {
+            StoreDao.createStore(store);
+        } catch (Exception e) {
+            System.err.println("Error al crear la tienda: " + e.getMessage());
+        }
+    }
+    
+    public List<Store> getAllStores() {
+         try {
+             return StoreDao.getAllStores();
+         } catch (Exception e) {
+             System.err.println("Error al obtener las tiendas: " + e.getMessage());
+             return null;
+         }
+     }
+    
+    public void updateStore(Store store) {
+         try {
+             storeDao.updateStore(store);
+         } catch (Exception e) {
+             System.err.println("Error al actualizar la tienda: " + e.getMessage());
+         }
+     }
+    
+    public void deleteStore(int store_id) {
+         try {
+             storeDao.deleteStore(store_id);
+         } catch (Exception e) {
+             System.err.println("Error al eliminar la tienda: " + e.getMessage());
+         }
+     }
+
+    public boolean isStoreNameExists(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
--- Anime Expo/src/Model/Store.java
+++ Anime Expo/src/Model/Store.java
@@ -9,5 +9,40 @@ package Model;
  * @author Juan Felipe Rubio
  */
 public class Store {
+    private int store_id;
+    private String name;
+    private String description;
+    private int manager;
     
+    public Store (int store_id, String name, String description, int manager) {
+        this.store_id = store_id;
+        this.name = name;
+        this.description = description;
+        this.manager = manager;
+    }
+    
+    public int getStore_id() {
+        return store_id;
+    }
+    public void setStore_id (int store_id){
+        this.store_id = store_id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription (String description){
+        this.description = description;
+    }
+    public int getManager() {
+        return manager;
+    }
+    public void setManager (int manager){
+        this.manager = manager;
+    }
 }
