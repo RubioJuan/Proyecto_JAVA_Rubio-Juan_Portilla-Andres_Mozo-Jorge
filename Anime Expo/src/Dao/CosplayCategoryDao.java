b2828d96bca0aab987c8ea1ffd44a03fd18eb88d
Andre230906 <andresportillaart@gmail.com>
2024-08-28 17:21:03 -0500

    feat: :zap: CosplayCategory entity
    All the MVC modules of the CosplayCategory have been created.

--- Anime Expo/src/Dao/CosplayCategoryDao.java
+++ Anime Expo/src/Dao/CosplayCategoryDao.java
@@ -4,10 +4,49 @@
  */
 package Dao;
 
-/**
- *
- * @author Juan Felipe Rubio
- */
+
+import connection.MySQLDataSource;
+import Model.CosplayCategory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CosplayCategoryDao {
-    
+    private MySQLDataSource dataSource;
+
+    public CosplayCategoryDao() {
+        this.dataSource = new MySQLDataSource();
+    }
+
+    public List<CosplayCategory> getAllCategories() {
+        List<CosplayCategory> categories = new ArrayList<>();
+        String query = "SELECT * FROM Cosplay_Categories";
+        try (Connection con = dataSource.conectarMySQL();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                CosplayCategory category = new CosplayCategory(
+                        rs.getInt("id_category"),
+                        rs.getString("category_name")
+                );
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
+
+    public void addCategory(CosplayCategory category) {
+        String query = "INSERT INTO Cosplay_Categories (category_name) VALUES (?)";
+        try (Connection con = dataSource.conectarMySQL();
+             PreparedStatement pstmt = con.prepareStatement(query)) {
+            pstmt.setString(1, category.getCategoryName());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
