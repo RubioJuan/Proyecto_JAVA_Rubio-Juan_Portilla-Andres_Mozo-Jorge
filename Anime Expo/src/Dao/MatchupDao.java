cf05a615a3634bd4ce2b91ba540a3d2d4e743e55
Andre230906 <andresportillaart@gmail.com>
2024-08-28 19:46:02 -0500

    feat: :zap: Matchup Entity
    All MVC modules of the Mutchup entity were created.

--- Anime Expo/src/Dao/MatchupDao.java
+++ Anime Expo/src/Dao/MatchupDao.java
@@ -4,10 +4,54 @@
  */
 package Dao;
 
-/**
- *
- * @author Juan Felipe Rubio
- */
+import connection.MySQLDataSource;
+import Model.Matchup;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatchupDao {
-    
+    private MySQLDataSource dataSource;
+
+    public MatchupDao() {
+        this.dataSource = new MySQLDataSource();
+    }
+
+    public List<Matchup> getAllMatchups() {
+        List<Matchup> matchups = new ArrayList<>();
+        String query = "SELECT * FROM Matchups";
+        try (Connection con = dataSource.conectarMySQL();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                Matchup matchup = new Matchup(
+                        rs.getInt("id_matchup"),
+                        rs.getInt("id_round"),
+                        rs.getInt("id_participant1"),
+                        rs.getInt("id_participant2"),
+                        rs.getObject("winner", Integer.class)
+                );
+                matchups.add(matchup);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return matchups;
+    }
+
+    public void addMatchup(Matchup matchup) {
+        String query = "INSERT INTO Matchups (id_round, id_participant1, id_participant2, winner) VALUES (?, ?, ?, ?)";
+        try (Connection con = dataSource.conectarMySQL();
+             PreparedStatement pstmt = con.prepareStatement(query)) {
+            pstmt.setInt(1, matchup.getRoundId());
+            pstmt.setInt(2, matchup.getParticipant1Id());
+            pstmt.setInt(3, matchup.getParticipant2Id());
+            pstmt.setObject(4, matchup.getWinnerId(), Types.INTEGER);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
